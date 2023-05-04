7cbfe29b424927082142728e312247cc02538b99
Ali503-7 <alimoali354@gmail.com>
2023-05-04 12:22:09 +0300

    build the vans padge

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Nav from "./components/NavBar/Nav";
 import Footer from "./components/Footer/Footer";
+import Vans from "./Pages/Vans";
 export default function App() {
   return (
     <div className="font-sans flex flex-col min-h-screen overflow-x-hidden">
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
       <footer>
         <Footer />
---
+++ src/Pages/Vans.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import "../assets/Server";
+
+const Vans = () => {
+  const [vans, setVans] = useState();
+
+  useEffect(() => {
+    const fetchVans = async () => {
+      try {
+        const response = await fetch("/api/vans");
+        const data = await response.json();
+        setVans(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchVans();
+  }, []);
+
+  const cars = () => {
+    if (vans) {
+      return vans.vans.map((car) => {
+        const carTypeStyle = {
+          background: ""
+        };
+        if (car.type == "simple") {
+          carTypeStyle.background = "#E17654";
+        } else if (car.type == "rugged") {
+          carTypeStyle.background = "#115E59";
+        } else (
+          carTypeStyle.background = "black"
+        )
+          return (
+            <div key={car.id} className="">
+              <div className=" rounded-xl overflow-hidden">
+                <img src={car.imageUrl} alt={car.name} />
+              </div>
+              <div className="flex justify-between h-[35px]">
+                <h3 className="text-Headers font-semibold text-[20px]">
+                  {car.name}
+                </h3>
+                <div className="flex flex-col">
+                  <span className="text-Headers font-semibold text-[20px]">
+                    ${car.price}
+                  </span>
+                  <span className="text-sm text-Headers relative bottom-2">
+                    /day
+                  </span>
+                </div>
+              </div>
+              <div
+                style={{ background: carTypeStyle.background }}
+                className="w-fit text-BG px-3 py-1 rounded-md"
+              >
+                {car.type}
+              </div>
+            </div>
+          );
+      });
+    } else {
+      return <div>Loading</div>;
+    }
+  };
+
+  return (
+    <div className="container grid gap-4 grid-cols-2 md:grid-cols-3 my-2">
+      {cars()}
+    </div>
+  );
+};
+
+export default Vans;
