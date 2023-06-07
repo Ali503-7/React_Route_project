f44a79f7dfbdebae428401caa4ef70c29396ec98
Ali503-7 <alimoali354@gmail.com>
2023-06-07 23:08:08 +0300

    add logout button to remove the loggedin form the localStorage

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -32,12 +32,17 @@ const Sign_In = () => {
   const Error = useActionData();
   const navigate = useNavigation();
 
+  const [login, setLogin] = useState(localStorage.getItem("loggedin"));
+
   return (
     <div className="container flex flex-col items-center w-full">
-      <div className=" absolute left-50 bottom-28">
-        <div>Email: b@b.com</div>
-        <div>Password: p123</div>
-      </div>
+      {login ? null :(
+        <div className=" absolute left-50 bottom-28">
+          <div>Email: b@b.com</div>
+          <div>Password: p123</div>
+        </div>
+      ) }
+
       <h1 className="font-bold text-xl text-Headers mb-5">
         Sign in to your account
       </h1>
@@ -48,35 +53,50 @@ const Sign_In = () => {
         <h2 className="text-red-600 font-bold text-lg mb-3">{Error}</h2>
       )}
       <Form className="flex flex-col mb-3" method="post" replace>
-        <div className="border border-stone-800 rounded-md overflow-hidden mb-8">
-          <input
-            type="email"
-            name="email"
-            placeholder="Email address"
-            className="bg-white w-full outline-none px-3 py-2 border border-b-Headers"
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className="bg-white w-full outline-none px-3 py-2"
-          />
-        </div>
-        <button
-          className={
-            navigate.state === "submitting"
-              ? "bg-gray-500 py-3 font-bold text-md text-white rounded-xl"
-              : "bg-btn py-3 font-bold text-md text-white rounded-xl"
-          }
-          disabled={navigate.state === "submitting"}
-        >
-          {navigate.state === "submitting" ? "logging in..." : "Sign in"}
-        </button>
+        {login ? null : (
+          <div className="border border-stone-800 rounded-md overflow-hidden mb-8">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email address"
+              className="bg-white w-full outline-none px-3 py-2 border border-b-Headers"
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="bg-white w-full outline-none px-3 py-2"
+            />
+          </div>
+        )}
+        {login ? (
+          <p
+            className="bg-red-800 py-3 font-bold text-md text-white rounded-xl px-5 cursor-pointer"
+            onClick={() => {
+              setLogin(localStorage.removeItem("loggedin"));
+            }}
+          >
+            Sign Out
+          </p>
+        ) : (
+          <button
+            className={
+              navigate.state === "submitting"
+                ? "bg-gray-500 py-3 font-bold text-md text-white rounded-xl"
+                : "bg-btn py-3 font-bold text-md text-white rounded-xl"
+            }
+            disabled={navigate.state === "submitting"}
+          >
+            {navigate.state === "submitting" ? "logging in..." : "Sign in"}
+          </button>
+        )}
       </Form>
-      <div>
-        Dont't have an account ?{" "}
-        <button className="text-btn">Create one now</button>
-      </div>
+      {login ? null : (
+        <div>
+          Dont't have an account ?{" "}
+          <button className="text-btn">Create one now</button>
+        </div>
+      )}
     </div>
   );
 };
